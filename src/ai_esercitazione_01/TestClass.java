006a9ba8ab664972d1e9c86dddb1af6cebada03d
Thomas Vitale <thomasvitale93@gmail.com>
2017-03-26 12:53:48 +0200

    add filter for authentication

--- src/ai_esercitazione_01/TestClass.java
+++
@@ -1,8 +0,0 @@
-package ai_esercitazione_01;
-
-public class TestClass {
-	int getVoid(){
-		//prova prova prova
-		return 4;
-	}
-}
---
+++ src/ai_esercitazione_01/filters/AuthFilter.java
@@ -0,0 +1,52 @@
+package ai_esercitazione_01.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class AuthFilter
+ */
+@WebFilter("/AuthFilter")
+public class AuthFilter implements Filter {
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		if (request instanceof HttpServletRequest) {
+	         HttpSession session = ((HttpServletRequest)request).getSession();
+	         if (session.getAttribute("username") == null) { 
+	        	 HttpServletResponse res = (HttpServletResponse)response;
+	        	 res.sendRedirect("login.jsp");
+	         } else {
+	           chain.doFilter(request,response);
+	         }
+         }
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
