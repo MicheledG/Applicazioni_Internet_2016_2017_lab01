df878faa4d5ae1fe2cf033da32fb80e709244aea
MicheledG <chiefzephyr@gmail.com>
2017-03-30 20:04:41 +0200

    Synchronized
    - LogoutServlet

--- src/ai_esercitazione_01/controllers/LogoutServlet.java
+++ src/ai_esercitazione_01/controllers/LogoutServlet.java
@@ -5,6 +5,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 /**
@@ -17,9 +19,15 @@ public class LogoutServlet extends HttpServlet {
     public static final String URL = "logout";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getSession().invalidate();
-        request.getSession(true);
-        request.getRequestDispatcher("index.jsp").forward(request, response);
+        
+    	HttpSession session = request.getSession();
+    	
+    	synchronized (session) {
+    		session.invalidate();
+    		request.getSession(true);
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+    	}
+    
     }
 
 }
